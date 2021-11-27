84647484b066c93645c7fe722f161cae03a1be87
Gabriel-WH <gabrielwhartmann@gmail.com>
2021-11-27 20:29:54 -0300

    tentativa

--- solucao-q10-GabrielHartmann.R
+++ solucao-q10-GabrielHartmann.R
@@ -3,24 +3,24 @@
 #Questão 7 com os valores alterados
 
 temp = 30
-UR = 30
+rh = 30
 
-wet_bulb_temperature = temp * atan(0.151977 * (UR + 8.313659)**(1/2) ) + atan(temp + UR) -
-  atan(UR - 1.676331) + 0.00391838 * (UR**(3/2) ) * atan(0.023101 * UR) - 4.686035
+wet_bulb_temperature = temp * atan(0.151977 * (rh + 8.313659)**(1/2) ) + atan(temp + rh) -
+  atan(rh - 1.676331) + 0.00391838 * (rh**(3/2) ) * atan(0.023101 * rh) - 4.686035
 
 wet_bulb_temperature
 
 #Questão 8 com os valores alterados
 
-SI = 13
-CL = 37
-Db = 1.3
-Me = 21
+si = 13
+cl = 37
+db = 1.3
+me = 21
 
-x14 = -1.05501 + 0.0650857*(SI)
-x15 = -2.07588 + 0.0423954*(CL)
-x16 = -6.03402 + 4.80572*(Db)
-x17 = -2.18409 + 8.84963*(Me)/100
+x14 = -1.05501 + 0.0650857*(si)
+x15 = -2.07588 + 0.0423954*(cl)
+x16 = -6.03402 + 4.80572*(db)
+x17 = -2.18409 + 8.84963*(me)/100
 z9 = 0.175202 + 1.18513*(x17) - 0.0996042*(x17)**2 + 0.327915*(x16) - 0.0758657*(x16)**2
 z10 = 0.929344*(z9) + 0.132519*(x14)
 
@@ -43,3 +43,5 @@ theta_1500kpa
 round(x = theta_10kpa, digits = 2)
 round(x = theta_33kpa, digits = 2)
 round(x = theta_1500kpa, digits = 2)
+
+sessionInfo()
